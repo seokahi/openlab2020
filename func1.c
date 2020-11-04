da0521388eb9a0fcea2ac2e502f962632451718a
kahi seo <skh9797@naver.com>
2020-11-03 22:27:05 -0800

    add

--- func1.c
+++ func1.c
@@ -5,3 +5,7 @@ void foo()
 {
 	printf("hello foo\n");
 }
+void mystablefunction()
+{
+	printf("haha");
+}
--- include/funcs.h
+++ include/funcs.h
@@ -1,4 +1,5 @@
 #ifndef __FUNCS_H__
 #define __FUNCS_H__
 void foo();
+void mystablefunction();
 #endif
